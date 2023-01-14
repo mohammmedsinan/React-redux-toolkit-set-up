4667b68918845d5ea97a660b8c5e2a0ce892b93c
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2023-01-14 13:20:20 +0300

    add reducer

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import counterReducer from './model'
+
+const store = configureStore({
+  reducer: { counter: counterReducer }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
